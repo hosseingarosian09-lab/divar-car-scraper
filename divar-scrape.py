ff773cf21563bc6744243bc15e4a981b1bdcd558
Hossein Garosian <hosseingarosian09@gmail.com>
2026-02-08 04:31:28 +0330

    some lines for finding kilometer,made year and color . blocks to 4 space (tab) .

--- divar-scrape.py
+++ divar-scrape.py
@@ -16,13 +16,27 @@ for card in cards:
 
 #finding each element in each page 
 for link in links :
-  request = requests.get(link,timeout=5)
-  soup = BeautifulSoup(request.text , "html.parser")
+    request = requests.get(link,timeout=5)
+    soup = BeautifulSoup(request.text , "html.parser")
 
-  titel_brand = None
-  try:
-    titel_brand = soup.find(class_="kt-unexpandable-row__action kt-text-truncate").text
-  except TypeError:
-    titel_brand = soup.find(class_="kt-base-row__end kt-unexpandable-row__value-box").text
-  else : 
+    # finding titel and brand 
     titel_brand = None
+    try:
+      titel_brand = soup.find(class_="kt-unexpandable-row__action kt-text-truncate").text
+    except TypeError:
+      titel_brand = soup.find(class_="kt-base-row__end kt-unexpandable-row__value-box").text
+    else : 
+      titel_brand = None
+
+    # finding kilometer,made year and color 
+    try:
+      info = soup.find_all(class_="kt-group-row-item kt-group-row-item__value kt-group-row-item--info-row")
+      if info :
+        kilometer = info[0].text
+        year = info[1].text
+        color = info[2].text
+    except:
+      kilometer = kilometer if kilometer else None
+      year = year if year else None
+      color = color if color else None
+
