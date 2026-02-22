2c800f0700c51113d16cf0bee4e2f6ec526075f9
hossein <hosseingarosian09@gmail.com>
2026-02-23 01:56:02 +0330

    wrote a function to store data as a CSV file in the given path

--- src/storage_CSV_and_JSON.py
+++ src/storage_CSV_and_JSON.py
@@ -11,3 +11,30 @@ def get_filename(folder=".", format="csv"):
     # Build full path
     full_path = os.path.join(folder, filename)
     return full_path
+
+def store_data_to_csv(items, filename=None, folder="."):
+    """Append one or many items to CSV."""
+    
+    fieldnames = [
+        'title_brand', 'kilometer', 'year', 'color', 'gearbox',
+        'fule', 'price', 'body_condition', 'discription', 'pictuer', 'link'
+    ]
+
+    if filename is None:
+        filename = get_filename(folder=folder, format="csv")
+
+    # Make sure the folder exists
+    os.makedirs(folder, exist_ok=True)
+
+    if not isinstance(items, (list, tuple)):
+        items = [items]
+
+    file_exists = os.path.isfile(filename) and os.path.getsize(filename) > 0
+
+    with open(filename, 'a', newline='', encoding='utf-8') as f:
+        writer = csv.DictWriter(f, fieldnames=fieldnames, quoting=csv.QUOTE_ALL)
+        
+        if not file_exists:
+            writer.writeheader()
+        
+        writer.writerows(items)
