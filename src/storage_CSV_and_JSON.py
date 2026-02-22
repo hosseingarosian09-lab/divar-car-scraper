8cd5e233b8bb4bcbcd0c8226a674bf44ed065061
hossein <hosseingarosian09@gmail.com>
2026-02-23 01:54:20 +0330

    wrote a function to make a path and filename: name is  based on format ("csv"or"json) and current date and time

--- src/storage_CSV_and_JSON.py
+++ src/storage_CSV_and_JSON.py
@@ -1,3 +1,13 @@
 import csv
 import datetime
 import os
+
+def get_filename(folder=".", format="csv"):
+    
+    current_date = datetime.datetime.now().strftime("%Y-%m-%d")
+    current_time = datetime.datetime.now().strftime("%H-%M-%S")
+    filename = f"ads_{current_date}_({current_time}).{format}"
+    
+    # Build full path
+    full_path = os.path.join(folder, filename)
+    return full_path
